82120b400d4587ae90241ea054f0c9c50b6dcd23
Lucas <lucaschiavini@hotmail.com>
2021-09-13 23:53:16 -0300

    String utils

---
+++ lib/assembler/include/stringUtils.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef STRINGUTILS_H
+#define STRINGUTILS_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+
+typedef std::vector< std::string> ListOfStrings;
+
+template <typename Out>
+
+void split(const std::string &s, char delim, Out result);
+std::vector<std::string> split(const std::string &s, char delim);
+std::string getListAsString(ListOfStrings vectorList);
+
+#endif
---
+++ lib/assembler/src/stringUtils.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <stringUtils.h>
+
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim)) {
+        *result++ = item;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
+
+std::string getListAsString(ListOfStrings vectorList) {
+    std::string listAsString;
+    for (uint16_t i=0; i < vectorList.size(); i++) {
+        if(i==0) {
+            listAsString.append(("%d " , vectorList.at(i)));
+        } else{
+            listAsString.append(" -> ");
+            listAsString.append(("%d" , vectorList.at(i)));
+        }
+    }
+    return listAsString; 
+}
