10029710226e6f12a83a5506f43535a8b8349621
Lucas <lucaschiavini@hotmail.com>
2021-09-14 00:29:31 -0300

    Starting to remake readToken

--- lib/assembler/include/assembler.h
+++ lib/assembler/include/assembler.h
@@ -8,8 +8,11 @@
 #include <symboltable.h>
 
 typedef std::vector< std::string> ListOfStrings;
-typedef std::vector< std::tuple<uint16_t, std::vector< std::string>> > FileLines;
-typedef std::map<std::string, uint16_t> InstructionToNumber;
+typedef std::map<std::string, uint16_t> DirectiveToNumber;
+
+// address opcode/value arg1 arg2 lineOriginalFile 
+typedef std::tuple< std::string, std::string, std::string, uint16_t > OpcodeArgsLine;
+typedef std::map<std::string, OpcodeArgsLine > FileLines;
 
 class Assembler {
     public:
@@ -17,7 +20,7 @@ class Assembler {
         ~Assembler();
         void assembleFile();
     private:
-        InstructionToNumber instructionToOpcode = {
+        DirectiveToNumber instructionToOpcode = {
             {"SPACE", 0},
             {"ADD", 1},
             {"SUB", 2},
@@ -34,7 +37,7 @@ class Assembler {
             {"OUTPUT", 13},
             {"STOP", 14}
         };
-        InstructionToNumber instructToSizeInMemory = {
+        DirectiveToNumber instructToSizeInMemory = { // shows Number of arguments valueMap.second-1 
             {"ADD", 2},
             {"SUB", 2},
             {"MULT", 2},
@@ -49,14 +52,15 @@ class Assembler {
             {"INPUT", 2},
             {"OUTPUT", 2},
             {"STOP", 1},
-            {"CONST", 1}
         };  
-            const std::map<std::string, uint8_t> dataToSizeInMemory = {
-            {"SPACE", 1}, // TODO: CHANGE THIS
+        DirectiveToNumber dataToSizeInMemory = {
+            {"SPACE", 1},
+            {"CONST", 1}, // TODO: CHANGE THIS
         };  
 
 
         SymbolTable symbolTable;
+        FileLines fileLineTable;
 
         std::fstream * sourceCode;
         std::fstream * assemblingCode; // TODO: DO I NEED *assemblingCode?
@@ -66,11 +70,16 @@ class Assembler {
         std::uint16_t currentLine = 0;
         uint16_t currentAddress = 0;
         std::string currentToken = "";
+        bool isLabelFlag = false;
+        bool isInstructionFlag = false;
+        bool isConstantFlag = false;
+        
 
         std::string fileName = "binComments.asm";
         
         // TESTING
         void printsMaps();
+        
         // END OF TESTING
 
 
@@ -81,15 +90,33 @@ class Assembler {
         
         // TOKEN
         void readToken();
+        void processToken(char currentChar);
+        
+        bool isNumber(std::string stringToTest);
         std::string readUntilEndToken(); 
         bool isInstruction(std::string token);
+        bool isDataDirective(std::string token);
         std::string getOpCode(std::string token);
         bool isValidLabel(std::string token);
         bool isDefinition(std::string token);
+        bool isValidInstruction(std::string token);
+        void operatesLabel(std::string label);
+        void operatesInstruction(std::string instruction);
+        void operatesConstant(std::string constant);
+        
+        uint16_t getsSizeVectorSpace();
+
         // END TOKEN
 
+        void putOnFileLineTable(std::string value);
+        void updatesCurrentAddress(
+            DirectiveToNumber mapToSizeInMemory,
+            uint16_t sizeVector,
+            bool isCONST
+        );
+
         void updatesAllUsedPositions();
-        void updatesAssembledCodeAtPosition(uint16_t position, uint16_t addressValue);
+        void updatesAssembledCodeAtAddress(uint16_t addressValue);
         ListOfStrings getUsedPositionsOfLabel(std::string label);
         void addsToUsedPosition(std::string label, uint16_t address);
         void readTillNextSectionOrEOF();
--- lib/assembler/src/assembler.cpp
+++ lib/assembler/src/assembler.cpp
@@ -1,7 +1,11 @@
 #include <assembler.h>
+#include <stringUtils.h>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <regex>
+#include <sstream>
+
 
 Assembler::~Assembler(){}
 Assembler::Assembler(std::fstream *source, std::string fileName){
@@ -12,7 +16,7 @@ Assembler::Assembler(std::fstream *source, std::string fileName){
 }
 
 void Assembler::printsMaps() {
-    InstructionToNumber::iterator iteratorMap;
+    DirectiveToNumber::iterator iteratorMap;
     std::cout << "__________INSTRUCTION TO OPCODE___________"<<std::endl;
     for(
         iteratorMap = this->instructionToOpcode.begin(); 
@@ -41,98 +45,261 @@ void Assembler::printsMaps() {
 }
 
 
-void Assembler::onePassAlgorithm(){
+void Assembler::updatesCurrentAddress(
+    DirectiveToNumber mapToSizeInMemory,
+    uint16_t sizeVector,
+    bool isCONST
+) {
+    if (!this->currentToken.empty()) {
+        if (isCONST) {
+            this->currentAddress += 1;
+        } else {
+            this->currentAddress += mapToSizeInMemory[this->currentToken] + sizeVector;
+        }
+    }
+}
 
-    uint16_t position = 0;
-    std::string label;
-    uint16_t addressValue;
-    bool setDefined; 
-    ListOfStrings listOfUse;
+bool Assembler::isValidLabel(std::string token) {
+    // TODO isValidLabel
+    return false;
+}
 
-    this->readToken();
-    
+void Assembler::operatesLabel(std::string label) {
+    bool setDefined = false; 
+    uint16_t addressValue = this->currentAddress;
     bool isDefined = this->symbolTable.isDefined(this->currentToken);
     bool isDefinition = this->isDefinition(this->currentToken);
+    ListOfStrings listOfUse;
 
-    while( !this->isEOF() ) {
+    this->isValidLabel(label);
 
-        
+    if(!this->symbolTable.contains(this->currentToken)) {
+        this->symbolTable.adds(label, addressValue, setDefined, listOfUse);
+    }
+    if(isDefined) {
+        std::uint16_t addressValue = this->symbolTable.getsAddressValue(label);
+        this->updatesAssembledCodeAtAddress(addressValue);
+    } else if(isDefinition){
+        setDefined = true;
+        updatesAllUsedPositions();
+    } else {
+        addsToUsedPosition(label, this->currentAddress);
+    }
+    // TODO operatesLabel
+    std::cout << this->currentToken <<"\t\t" << this->currentLine <<"\t" << this->currentAddress <<std::endl;
+
+}
+
+bool Assembler::isValidInstruction(std::string token) {
+    // TODO isValidLabel
+    return false;
+}
+
+uint16_t Assembler::getsSizeVectorSpace() {
+    return 0;
+}
+
+void Assembler::operatesInstruction(std::string instruction) {
+    this->isValidInstruction(instruction);
+    DirectiveToNumber mapToSizeInMemory = this->dataToSizeInMemory;
+    uint16_t sizeVector = 0;
+    bool isCONST = 0;
+    if(this->currentToken == "SPACE") {
+        sizeVector = this->getsSizeVectorSpace();
+    } else if(this->currentToken == "CONST") {
+        isCONST = 1;
+    } else {
+        mapToSizeInMemory = this->instructToSizeInMemory;
+    }
+    this->updatesCurrentAddress(mapToSizeInMemory, sizeVector, isCONST);
+}
+
+void Assembler::operatesConstant(std::string constant) {
+    this->putOnFileLineTable(constant);
+    std::cout << this->currentToken <<"\t\t" << this->currentLine <<"\t" << this->currentAddress <<std::endl;
+}
+
+void Assembler::putOnFileLineTable(std::string value) {
+
+}
+       
+
+void Assembler::onePassAlgorithm(){
+    this->readToken();
+    bool isDefined = this->symbolTable.isDefined(this->currentToken);
+    bool isDefinition = this->isDefinition(this->currentToken);
+    std::cout << "TOKEN\t\t LINE\t ADDRESS\t"<<std::endl;
+            
+    while( !this->isEOF() ) {    
         if( isDefined && isDefinition ) {
             throw ("Semantic Exception at line %d", currentLine);
         }
-        if(!this->symbolTable.contains(this->currentToken)) {
-            this->symbolTable.adds(label, addressValue, isDefined, listOfUse);
-        }
-        if(isDefined) {
-            std::uint16_t addressValue = this->symbolTable.getsAddressValue(label);
-            this->updatesAssembledCodeAtPosition(position, addressValue);
-        } else if(isDefinition){
-            setDefined = true;
-            updatesAllUsedPositions();
-        } else {
-            addsToUsedPosition(label, this->currentAddress);
-        }
-
-        
-        this->currentLine++;
-        this->currentAddress += this->instructToSizeInMemory[label];
+        if(this->isLabelFlag) {
+            this->operatesLabel(this->currentToken);
+        } else if(this->isInstructionFlag) {
+            this->operatesInstruction(this->currentToken);
+        } else if(this->isConstantFlag) {
+            this->operatesConstant(this->currentToken);
+        }        
         this->readToken();
     }
 }
 
 bool Assembler::isDefinition(std::string token){
-    return false;
+    return false; // TODO isDefinition
 }
 
 bool Assembler::isEOF() {
     return this->sourceCode->eof();
 }
 
-void Assembler::readToken() {
-    char c;
-    bool endWhile = false;
-    this->currentToken.clear();
-    std::string currentChar;
-    while(!endWhile){
-        this->sourceCode->get(c);
-        endWhile = c == ':' 
-        || c == ' ' 
-        || c == ';'
-        || c == ','
-        || c == '\n';
-
-        if(c == ':') {
-            
-        } 
-        else if(c == ' ') {
-
-        } else if(c == '\n') {
-
-        } else if(c == ',') {
-
-        } else if(c ==  ';') {
-            std::string comment;
-            std::getline(*(this->sourceCode), comment);
-        } else if(!this->isEOF()) {
-            currentChar = ("%c", c);
-            this->currentToken += currentChar;            
-        }
-
-        if(endWhile) {
-            // this->sourceCode->unget();
-        } else if(this->isEOF()) {
-            endWhile = true;
-        }
-    }
-
-    if (!this->currentToken.empty()) {
-        std::cout << "Current Token: " << this->currentToken <<std::endl;
-    }
-    
-    // TODO readToken
+bool Assembler::isNumber(std::string stringToTest) {
+    std::regex rx("([0-9])");
+    std::smatch m;
+    bool isNumber = std::regex_match(stringToTest, rx);
+    return isNumber;
 }
 
-void Assembler::updatesAssembledCodeAtPosition(uint16_t position, uint16_t addressValue) {
+void Assembler::processToken(char currentChar) {
+
+    this->isLabelFlag = false;
+    this->isInstructionFlag = false;
+    this->isConstantFlag = false;
+
+    if(this->isNumber(this->currentToken)) {
+        this->isConstantFlag =  true;
+    } else {
+        if(currentChar == ':') {
+            this->isLabelFlag = true;
+        } 
+        else if(currentChar == ' ') {
+            if(this->isInstruction(this->currentToken)) {
+                this->isInstructionFlag = true;
+            }
+        } else if(currentChar == '\n') {
+            this->isLabelFlag = true;
+        } else if(currentChar == ',') {
+
+        } else if(currentChar ==  ';') {
+            std::string comment;
+            std::getline(*(this->sourceCode), comment);
+        } else if(currentChar ==  '\n') {
+            this->currentLine++;
+        } 
+    }   
+
+    // if(this->isLabelFlag) {
+    //     std::cout << "isLabel"<<std::endl;
+    // } else if(this->isInstructionFlag) {
+    //     std::cout << "isInstruction"<<std::endl;
+    // } else if(this->isConstantFlag) {
+    //     std::cout << "isConstant"<<std::endl;
+    // }
+}
+
+bool Assembler::isInstruction(std::string token) {
+    DirectiveToNumber instructionMap = this->instructionToOpcode;
+    if(instructionMap.find(token) != instructionMap.end()) {
+        return true;
+    }
+    return false;
+}
+
+bool Assembler::isDataDirective(std::string token) {
+    DirectiveToNumber instructionMap = this->instructionToOpcode;
+    if(instructionMap.find(token) != instructionMap.end()) {
+        return true;
+    }
+    return false;
+}
+
+void Assembler::readToken() {
+    std::string currentLine,
+                typeOfSection,
+                comment,
+                labelDef,
+                opcodeValue,
+                arg1,
+                arg2;
+    FileLines currentFileLine;
+    ListOfStrings fromSplit;
+    while(std::getline(*(this->sourceCode), currentLine)) {
+        currentLine = removeMultipleSpaces(currentLine);
+        // COMMENTS
+        fromSplit = split(currentLine, ';');
+        currentLine = fromSplit.at(0);
+
+        //TYPEOFSECTION
+        
+        fromSplit = split(currentLine, ':');
+        if(fromSplit.size() > 1) { // FOUND LABEL DEF
+            labelDef = fromSplit.at(0);
+            std::cout << "Label    \t" << labelDef <<std::endl;
+            currentLine = fromSplit.at(1);
+        } else { // SAME STRING
+            currentLine = fromSplit.at(0);
+        }
+
+
+        // LABEL DEF
+
+        std::cout << "currentLine    \t" << currentLine <<std::endl;
+        fromSplit = split(currentLine, ':');
+        if(fromSplit.size() > 1) { // FOUND LABEL DEF
+            labelDef = fromSplit.at(0);
+            std::cout << "Label    \t" << labelDef <<std::endl;
+            currentLine = fromSplit.at(1);
+        } else { // SAME STRING
+            currentLine = fromSplit.at(0);
+        }
+
+        // SPACES
+        fromSplit = split(currentLine, ' ');
+        std::cout << "fromSplit SPACE\t" << getListAsString(fromSplit) << "\tSIZE LIST: " << fromSplit.size() <<std::endl;
+
+        // if(fromSplit.size() > 1) { // FOUND LABEL DEF
+        //     labelDef = fromSplit.at(0);
+        //     std::cout << "Label    \t" << labelDef <<std::endl;
+        //     currentLine = fromSplit.at(1);
+        // } else { // SAME STRING
+        //     currentLine = fromSplit.at(0);
+        // }
+
+
+
+
+
+        // std::cout << "fromSplit    \t" << getListAsString(fromSplit) << "\tSIZE LIST: " << fromSplit.size() <<std::endl;
+
+    }
+
+
+
+    // char c;
+    // bool exitChar = false;
+    // this->currentToken.clear();
+    // std::string currentCharString;
+    // while(!exitChar){
+    //     this->sourceCode->get(c);
+    //     exitChar = c == ':' 
+    //     || c == ' ' 
+    //     || c == ';'
+    //     || c == ','
+    //     || c == '\n';
+    //     if(exitChar) {
+    //         this->processToken(c);
+    //     } else {
+    //         if(!this->isEOF()) { 
+    //             currentCharString = ("%c", c);
+    //             this->currentToken += currentCharString;  
+    //         } else {
+    //             exitChar = true;
+    //         }
+    //     }
+    // }
+}
+
+void Assembler::updatesAssembledCodeAtAddress(uint16_t addressValue) {
     // TODO updatesAssembledCodeAtPosition
 }
 
