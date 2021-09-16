56c16d490d303608ae3b601807605641dfe7d37f
Lucas <lucaschiavini@hotmail.com>
2021-09-15 22:23:44 -0300

    Gets all lines in map structure

--- lib/simulator/include/simulator.h
+++ lib/simulator/include/simulator.h
@@ -3,10 +3,29 @@
 #define SIMULATOR_H
 
 #include <fstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <map>
+#include <fstream>
 #include <stringUtils.h>
 
 
 typedef std::vector< std::string> ListOfStrings;
+typedef std::map<std::string, uint16_t> DirectiveToNumber;
+typedef std::map<std::string, std::string> OpCodeToInstruction;
+
+typedef std::tuple<
+    uint16_t, 
+    std::string,
+    std::string,
+    std::string
+> AddressOpcodeArgsLine;
+
+
+// map<[PC] [PC, opcode, arg1, arg2]>
+// address opcode/value arg1 arg2 lineOriginalFile 
+typedef std::map<uint16_t, AddressOpcodeArgsLine > FileLines;
 
 class Simulator {
     public:
@@ -15,12 +34,64 @@ class Simulator {
         void simulate();
 
     private:
+        uint16_t PC = 0;
+        uint16_t ACC = 0;
+        uint16_t MEM = 0;
+        bool reachedSTOP = false;
+        
+        OpCodeToInstruction opcodeToInstruction = {
+            {"0", "SPACE"},
+            {"1", "ADD"},
+            {"2", "SUB"},
+            {"3", "MULT"},
+            {"4", "DIV"},
+            {"5", "JMP"},
+            {"6", "JMPN"},
+            {"7", "JMPP"},
+            {"8", "JMPZ"},
+            {"9", "COPY"},
+            {"10", "LOAD"},
+            {"11", "STORE"},
+            {"12", "INPUT"},
+            {"13", "OUTPUT"},
+            {"14", "STOP"}
+        };
+        DirectiveToNumber instructToSizeInMemory = { // shows Number of arguments valueMap.second-1 
+            {"ADD", 2},
+            {"SUB", 2},
+            {"MULT", 2},
+            {"DIV", 2},
+            {"JMP", 2},
+            {"JMPN", 2},
+            {"JMPP", 2},
+            {"JMPZ", 2},
+            {"COPY", 3},
+            {"LOAD", 2},
+            {"STORE", 2},
+            {"INPUT", 2},
+            {"OUTPUT", 2},
+            {"STOP", 1},
+            {"SPACE", 1},
+            {"CONST", 1}
+        };  
+
         std::string fileContentsStrint;
         ListOfStrings fileContents;
+        FileLines fileLines;
+
         std::string fileName;
         std::string fileDir;
         std::fstream input;
         void setFileContents();
+        void setFileLines();
+        void printFileLines();
+        void printTuple(
+            uint16_t pc,
+            std::string instruction,
+            std::string arg1,
+            std::string arg2
+        );
+
 
 };
 
--- lib/simulator/src/simulator.cpp
+++ lib/simulator/src/simulator.cpp
@@ -17,7 +17,8 @@ Simulator::Simulator(std::string fileName){
 }
 void Simulator::simulate() {
     this->setFileContents();
-
+    this->setFileLines();
+    this->printFileLines();
 }
 
 void Simulator::setFileContents() {
@@ -28,4 +29,105 @@ void Simulator::setFileContents() {
     this->input.close();
     this->fileContents = split(this->fileContentsStrint, ' ');
     std::cout << "fileContents with [" << getListAsString(this->fileContents)  << "]" <<std::endl;
+    std::cout << "fileContents size [" << this->fileContents.size()  << "]" <<std::endl;
+}
+
+
+void Simulator::setFileLines() {
+    uint16_t pc = 0;
+    std::string instruction = "";
+    std::string arg1 = "";
+    std::string arg2 = "";
+    bool isCONST = false;
+
+    for(int i = 0; i < this->fileContents.size() ; i++) {
+        isCONST = false;
+        if(instruction == "STOP") isCONST = true;
+        instruction = "";
+        arg1 = "";
+        arg2 = "";
+        
+        // std::cout << "line at position = "<< i <<std::endl;
+        // std::cout << "this->fileContents[i] = "<< this->fileContents[i] <<std::endl;
+
+
+        if(isCONST) {
+            instruction = this->fileContents[i];
+            arg1 = "";
+            arg2 = "";
+            i+=1;
+        }
+        else if(this->opcodeToInstruction.find(this->fileContents[i]) != this->opcodeToInstruction.end()) {
+            instruction = this->opcodeToInstruction[this->fileContents[i]];
+            // std::cout << "instruction = "<< instruction <<std::endl;
+            uint16_t numOfArgs = this->instructToSizeInMemory[instruction] - 1;
+            for(int j = 1; j <=  numOfArgs; j++) {
+                if(j == 1) arg1 = this->fileContents[i + j];
+                else {
+                    arg2 = this->fileContents[i + j];
+                }                
+                std::cout << "j = "<< j <<std::endl;
+            }
+            i+= numOfArgs;
+            // std::cout << "numOfArgs = "<< numOfArgs <<std::endl;
+            // std::cout << "arg1 = "<< arg1 <<std::endl;
+            // std::cout << "arg2 = "<< arg2 <<std::endl;
+            // std::cout << "i = "<< i <<std::endl;
+        }
+        
+        this->fileLines[pc] = make_tuple(
+            pc,
+            instruction,
+            arg1,
+            arg2
+        );
+        this->printTuple(
+            pc,
+            instruction,
+            arg1,
+            arg2
+        );
+        
+        pc = i;
+        
+    }
+}
+
+void Simulator::printTuple(
+    uint16_t pc,
+    std::string instruction,
+    std::string arg1,
+    std::string arg2
+) {
+    std::cout << "map[" << pc << "] : "
+    << instruction << " " 
+    << arg1 << " " 
+    << arg2 << " "    
+    <<std::endl;
+}
+
+
+void Simulator::printFileLines() {
+    FileLines::iterator it = this->fileLines.begin();
+    uint16_t pc = 0;
+    std::string instruction = "";
+    std::string arg1 = "";
+    std::string arg2 = "";
+    while(it != this->fileLines.end()) {
+        
+        uint16_t key = it->first;
+        AddressOpcodeArgsLine currentLine = it->second;
+        pc = std::get<0>(currentLine);
+        instruction = std::get<1>(currentLine);
+        arg1 = std::get<2>(currentLine);
+        arg2 = std::get<3>(currentLine);
+        this->printTuple(
+            pc,
+            instruction,
+            arg1,
+            arg2
+        );
+        
+        it++;
+    }
 }
